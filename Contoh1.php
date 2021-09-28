c732ed09a5684e8f04345dc7dd75d46081fba28a
tamiajuwita <juwitamia31@gmail.com>
2021-09-28 16:29:24 +0700

    menambahkan Contoh1.php,Latihan1.php,Model_latihan1.php,view-latihan1.php

---
+++ Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller 
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Tamia Juwita
+            Saya tinggal di daerah Curug
+            olahraga yang saya sukai adalah
+            Bulutangkis";
+    }
+}
+?>
---
+++ view-latihan1.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+</head>
+
+<body>
+    Halo Kawan... Yuk kita belajar Web Programming!!!<br>
+    Nilai 1 = <?=$nilai1;?>
+    Nilai 2 = <?=$nilai2;?>
+    Ini hasil dari pemodelan dengan metode penjumlahan yaitu
+    <?=$nilai1 . " + " . $nilai2 . " = " . $hasil;?>
+
+</body>
+</html>
